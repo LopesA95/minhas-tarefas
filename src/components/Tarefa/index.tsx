2156bba3dfbc933d66f50a0d2a9d82b2dcd4fc34
Anderson Lopes <andersonlopes017.al@gmail.com>
2024-04-08 18:00:22 -0400

    ✨ Create the Button Swap State

--- src/components/Tarefa/index.tsx
+++ src/components/Tarefa/index.tsx
@@ -1,14 +1,35 @@
+import { useState } from 'react'
 import * as S from './styles'
 
-export const Tarefa = () => (
-  <S.Card>
-    <S.Title>Nome da tarefa</S.Title>
-    <S.Tags>Importante</S.Tags>
-    <S.Tags>Pendente</S.Tags>
-    <S.Description />
-    <S.ActionBar>
-      <S.Button>Editar</S.Button>
-      <S.Button>Remover</S.Button>
-    </S.ActionBar>
-  </S.Card>
-)
+type Props = {
+  title: string
+  priority: string
+  status: string
+  description: string
+}
+
+export const Tarefa = ({ description, priority, status, title }: Props) => {
+  const [editing, setEditing] = useState(false)
+
+  return (
+    <S.Card>
+      <S.Title>{title}</S.Title>
+      <S.Tags>{priority}</S.Tags>
+      <S.Tags>{status}</S.Tags>
+      <S.Description value={description} />
+      <S.ActionBar>
+        {editing ? (
+          <>
+            <S.Button>Salvar</S.Button>
+            <S.Button onClick={() => setEditing(false)}>Cancelar</S.Button>
+          </>
+        ) : (
+          <>
+            <S.Button onClick={() => setEditing(true)}>Editar</S.Button>
+            <S.Button>Remover</S.Button>
+          </>
+        )}
+      </S.ActionBar>
+    </S.Card>
+  )
+}
--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,24 +1,43 @@
 import { Tarefa } from '../../components/Tarefa'
 import { Container } from './styles'
 
+const tarefas = [
+  {
+    id: 1,
+    title: 'Estudar Typescript',
+    description: 'Tipagem de codigo',
+    priority: 'normal',
+    status: 'pendente'
+  },
+  {
+    id: 2,
+    title: 'Pagar a conta de internet',
+    description: 'Baixar fatura no gmail',
+    priority: 'Urgente',
+    status: 'concluída'
+  },
+  {
+    id: 3,
+    title: 'Ir para a academia',
+    description: 'Fazer o treino de B',
+    priority: 'importante',
+    status: 'pendente'
+  }
+]
+
 export const ListaDeTarefas = () => (
   <Container>
     <ul>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
+      {tarefas.map((t) => (
+        <li key={t.id}>
+          <Tarefa
+            description={t.description}
+            title={t.title}
+            status={t.status}
+            priority={t.priority}
+          />
+        </li>
+      ))}
     </ul>
   </Container>
 )
