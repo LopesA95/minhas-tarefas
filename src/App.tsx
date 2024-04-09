37fa6e5e47640551f33ae60d0eb58cfee70a82b1
Anderson Lopes <andersonlopes017.al@gmail.com>
2024-04-09 17:41:20 -0400

    ✨ Creating Application Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
 import { Provider } from 'react-redux'
-import { BarraLateral } from './containers/BarraLateral'
-import { ListaDeTarefas } from './containers/ListaDeTarefas'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import { Container, GlobalStyle } from './styles'
 
 import { store } from './store'
+import { Home } from './pages/Home'
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/novo',
+    element: <h1>Nova tarefa</h1>
+  }
+])
 
 export function App() {
   return (
     <Provider store={store}>
       <GlobalStyle />
       <Container>
-        <BarraLateral />
-        <ListaDeTarefas />
+        <RouterProvider router={routes} />
       </Container>
     </Provider>
   )
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import { ListaDeTarefas } from '../../containers/ListaDeTarefas'
+import { BarraLateral } from './../../containers/BarraLateral/'
+
+export const Home = () => (
+  <>
+    <BarraLateral />
+    <ListaDeTarefas />
+  </>
+)
